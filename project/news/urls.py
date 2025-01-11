8c0da0ffd65610641ddbec2e10c711a7317de7ec
Sergey89-N <Nehoroshkov76@mail.ru>
2025-01-11 19:03:04 +0500

    устранение ошибки
    Исправление неправильного написания as_view() в urls.py

--- project/news/urls.py
+++ project/news/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import NewsListView, NewsDetailView
 
 urlpatterns = [
-    path('news/', NewsListView.as_view, name='news_list'),
-    path('news/<int:pk/', NewsDetailView.as_view, name='news_detail'),
+    path('news/', NewsListView.as_view(), name='news_list'),
+    path('news/<int:pk/', NewsDetailView.as_view(), name='news_detail'),
 ]
