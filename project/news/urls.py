1e3496f3053a8ed90c8155b714be1292fcceab01
Sergey89-N <Nehoroshkov76@mail.ru>
2025-01-11 21:36:35 +0500

    Итоговое задание
    Итоговое задание 7.1

--- project/news/urls.py
+++ project/news/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import NewsListView, NewsDetailView
+from .views import (NewsListView, NewsDetailView, NewsSearchView, NewsCreateView, NewsUpdateView, NewsDeleteView,
+                    ArticleCreateView, ArticleUpdateView, ArticleDeleteView
+                    )
 
 urlpatterns = [
     path('news/', NewsListView.as_view(), name='news_list'),
     path('news/<int:pk/', NewsDetailView.as_view(), name='news_detail'),
+    path('news/search/', NewsSearchView.as_view(), name='news_search'),
+    path('news/create/', NewsCreateView.as_view(), name='news_create'),
+    path('news/<int:pk>/edit/', NewsUpdateView.as_view(), name='news_edit'),
+    path('news/<int:pk>/delete/', NewsDeleteView.as_view(), name='news_delete'),
+    path('articles/create/', ArticleCreateView.as_view(), name='article_create'),
+    path('articles/<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
+    path('articles/<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
 ]
--- project/news/views.py
+++ project/news/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.urls import reverse_lazy
 from .models import Post
 
 
@@ -7,9 +8,81 @@ class NewsListView(ListView):
     template_name = 'news/news_list.html'
     context_object_name = 'posts'
     queryset = Post.objects.filter(post_type='NW').order_by('-created_at')
+    paginate_by = 10
 
 
 class NewsDetailView(DetailView):
     model = Post
     template_name = 'news/news_detail.html'
     context_object_name = 'post'
+
+class NewsSearchView(ListView):
+    model = Post
+    template_name = 'news/news_search.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q') # поиск по названию
+        author = self.request.GET.get('author') # поиск по автору
+        date = self.request.GET.get('date') # поиск по дате
+
+        if query:
+            queryset = queryset.filter(title__icontains=query)
+        if author:
+            queryset = queryset.filter(author__user__username__icontains=author)
+        if date:
+            queryset = queryset.filter(created_at__gte=date)
+
+        return queryset.filter(post_type='NW').order_by('-created_at')
+
+
+class NewsCreateView(CreateView):
+    model = Post
+    template_name = 'news/news_create.html'
+    fields = ['title', 'content', 'categories'] # поля для создания новости
+    success_url = reverse_lazy('news_list') # перенаправление
+
+    def form_valid(self, form):
+        form.instance.post_type = 'NW'
+        form.instnce.author = self.request.user.author # устанавливаем автора
+        return super().form_valid(form)
+
+
+class NewsUpdateView(UpdateView):
+    model = Post
+    template_name = 'news/news_edit.html'
+    fields = ['title', 'content', 'categories']  # поля для редактирования
+    success_url = reverse_lazy('news_list')  # перенаправление
+
+
+class NewsDeleteView(DetailView):
+    model = Post
+    template_name = 'news/news_delete.html'
+    success_url = reverse_lazy('news_list')
+
+
+class ArticleCreateView(CreateView):
+    model = Post
+    template_name = 'news/article_create.html'
+    fields = ['title', 'content', 'categories']
+    success_url = reverse_lazy('news_list')
+
+    def form_valid(self, form):
+        form.instance.post_type = 'AR'
+        form.instance.author = self.request.user.author
+        return super().form_valid(form)
+
+
+class ArticleUpdateView(UpdateView):
+    model = Post
+    template_name = 'news/article_edit.html'
+    fields = ['title', 'content', 'categories']
+    success_url = reverse_lazy('news_list')
+
+
+class ArticleDeleteView(DetailView):
+    model = Post
+    template_name = 'news/article_delete.html'
+    success_url =reverse_lazy('news_list')
